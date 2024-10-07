c543f7558248b0b8dd07f707fc9a361d9a7ea47f
Tousif Hasan Lavlu <41795217+thlavlu@users.noreply.github.com>
2024-10-07 16:50:03 +0600

    update

--- scripts/update_publication.sh
+++ scripts/update_publication.sh
@@ -5,9 +5,9 @@
     # pip3 install -U academic==0.11.2
     #fi
 # fetch bib from dbpl
-    test -f /tmp/alan_publications.bib && rm -r /tmp/alan_publications.bib
-    curl "https://dblp.org/pid/141/9540.bib?param=1" -o /tmp/alan_publications.bib
-    import to content/publication
-    test -d content/publications && rm -r content/publications
-    academic import --bibtex /tmp/alan_publications.bib --publication-dir content/publications --overwrite
+    #test -f /tmp/alan_publications.bib && rm -r /tmp/alan_publications.bib
+    #curl "https://dblp.org/pid/141/9540.bib?param=1" -o /tmp/alan_publications.bib
+    #import to content/publication
+   # test -d content/publications && rm -r content/publications
+    #academic import --bibtex /tmp/alan_publications.bib --publication-dir content/publications --overwrite
 
